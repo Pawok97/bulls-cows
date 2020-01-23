bcd556326e6f11ec38345fd3b9adb09d588e5526
pawokbad <pashok97@hotmail.com>
2020-01-23 14:16:31 +0400

    git

---
+++ src/main/java/com/game/bullscows/BullsCowsApplication.java
@@ -0,0 +1,13 @@
+package com.game.bullscows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsCowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullsCowsApplication.class, args);
+	}
+
+}
